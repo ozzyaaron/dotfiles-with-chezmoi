e611e073caf9e2bd5281aa17136abdd363bbb9f0
Aaron Todd <ozzyaaron@gmail.com>
2024-09-29 10:21:11 -0600

    initial astronvim config

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
---if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -32,11 +32,6 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
-
-        -- Get autosaving on buffer changes etc going
-        autowriteall = true,
-        autowrite = true,
-        autocmd = "BufLeave,FocusLost * silent! wall",
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
