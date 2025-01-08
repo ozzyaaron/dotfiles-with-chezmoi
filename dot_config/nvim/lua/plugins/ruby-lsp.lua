86623cd281043fcbebfc37cf3ae3a08a37ed706d
Aaron Todd <ozzyaaron@gmail.com>
2025-01-08 15:29:05 -0700

    initial lazyvim config

---
+++ dot_config/nvim/lua/plugins/ruby-lsp.lua
@@ -0,0 +1,36 @@
+return {
+  -- Configure lspconfig
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   opts = {
+  --     ensure_installed = { "ruby_lsp" }, -- automatically install lsp
+  --   },
+  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          -- Ruby LSP configuration
+          cmd = { "bundle", "exec", "ruby-lsp" },
+          filetypes = { "ruby" },
+          root_dir = require("lspconfig.util").root_pattern("Gemfile", ".git"),
+          init_options = {
+            formatter = "auto",
+          },
+          settings = {
+            -- Add any specific settings here
+            ruby = {
+              lint = {
+                enable = true,
+              },
+              format = {
+                enable = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
