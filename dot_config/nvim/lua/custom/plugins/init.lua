3f76cff96dd021609c7c2a94bca35131ccdd3a43
Aaron Todd <ozzyaaron@gmail.com>
2025-02-06 08:35:43 -0700

    add vim-rails, auto-save, tmux-navigator

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'tpope/vim-rails',
+    name = 'vim-rails',
+    opts = {},
+    config = function() end,
+  },
+  {
+    'pocco81/auto-save.nvim',
+    name = 'auto-save.nvim',
+    opts = {},
+    config = function() end,
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    name = 'vim-tmux-navigator',
+    opts = {},
+    config = function() end,
+  },
+}
