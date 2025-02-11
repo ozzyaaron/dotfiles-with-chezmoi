4ee2359f29d2dc3a22dbe2d732e3cbee91a51203
Aaron Todd <ozzyaaron@gmail.com>
2025-02-11 09:22:34 -0700

    set tabs and stuff for neovim

--- dot_config/nvim/lua/custom/config.lua
+++ dot_config/nvim/lua/custom/config.lua
@@ -1,1 +1,6 @@
 vim.opt.wrap = false
+
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.shiftwidth = 2    -- Number of spaces to use for indentation
+vim.opt.tabstop = 2       -- Number of spaces for a tab
+vim.opt.softtabstop = 2   -- Number of spaces for a tab while editing
