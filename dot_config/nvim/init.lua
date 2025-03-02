8d5ec9d755634957a2dc43652fa21d54bf405f14
Aaron Todd <ozzyaaron@gmail.com>
2025-03-02 16:17:33 -0700

    Add Snacks with zen and some vim options

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -156,7 +156,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
-vim.opt.wrap = false
+require("custom.options")
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -229,8 +229,14 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  plugins = {
+    change_detection = {
+      enabled = false,
+      notify = false, -- Optional: disable notifications
+    },
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
---
+++ dot_config/nvim/lua/custom/options.lua
@@ -0,0 +1,10 @@
+vim.opt.wrap = false
+
+vim.opt.expandtab = true  -- Convert tabs to spaces
+vim.opt.shiftwidth = 2    -- Number of spaces to use for indentation
+vim.opt.tabstop = 2       -- Number of spaces for a tab
+vim.opt.softtabstop = 2   -- Number of spaces for a tab while editing
+
+-- Try to prevent tabstop and other overrides
+vim.opt.modeline = false
+vim.g.editorconfig = false
