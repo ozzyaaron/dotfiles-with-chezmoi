10f12865b9701cbb36869378a75f6b4ba2aebfce
Aaron Todd <ozzyaaron@gmail.com>
2025-03-28 15:49:03 -0600

    telescope ignores vendor folder

--- dot_config/nvim/lua/custom/plugins/telescope.lua
+++ dot_config/nvim/lua/custom/plugins/telescope.lua
@@ -49,11 +49,9 @@ return {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          file_ignore_patterns = { "vendor" },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
