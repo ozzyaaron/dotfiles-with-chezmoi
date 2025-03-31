5e69635e0750ca3494a008b5ab20055277c7f41a
Aaron Todd <ozzyaaron@gmail.com>
2025-03-31 07:57:45 -0600

    remove gitsigns setup from init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -236,19 +236,6 @@ require('lazy').setup("custom.plugins", {
   change_detection = {
     notify = false, -- Optional: disable notifications
   },
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
   -- Highlight todo, notes, etc in comments
   -- { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
