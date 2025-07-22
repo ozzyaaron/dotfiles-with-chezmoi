5430ecbdb6870e995da1e644507359bbe729804f
Aaron Todd <ozzyaaron@gmail.com>
2025-07-22 15:13:45 -0600

    updates for Tabular to format cucumber tables

--- dot_config/nvim/lua/custom/plugins/tabular.lua
+++ dot_config/nvim/lua/custom/plugins/tabular.lua
@@ -1,6 +1,17 @@
 return {
   {
     'godlygeek/tabular',
-    config = function() end,
+    config = function()
+      -- Function to align on pipe and shift left by 1 character
+      local function align_and_shift()
+        vim.cmd('Tabularize /|')
+        vim.cmd('normal! gv')
+        vim.cmd("'<,'>s/^.//")
+      end
+
+      -- Visual mode mapping for aligning on pipe character
+      vim.keymap.set('v', 'fct', align_and_shift,
+        { desc = 'Align selection on pipe character and remove leading space' })
+    end,
   },
 }
